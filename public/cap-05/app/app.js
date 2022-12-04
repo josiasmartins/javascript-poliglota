4b73ea753d6bad04d3fcbda163ef33e63d5a1d6a
Caitano <josiasmartinscaitano@gmail.com>
2022-12-04 19:26:59 -0300

    feat: aplicando decorators direto na classe

--- public/cap-05/app/app.js
+++ public/cap-05/app/app.js
@@ -2,14 +2,6 @@ import { Person } from './models/person.js';
 import { logExecutionTime, inspectMethod } from './decorators/decorators.js';
 import { decorate } from './infra/decorate.js';
 
-decorate(
-    Person,
-    {
-        speak: [logExecutionTime, inspectMethod],
-        getFullName: logExecutionTime
-    }
-)
-
 const person = new Person('Flávio', 'Almeida');
 // person.getFullName();
 person.speak('Tudo bem?')
--- public/cap-05/app/decorators/decorators.js
+++ public/cap-05/app/decorators/decorators.js
@@ -1,16 +1,20 @@
 export const logExecutionTime = (method, property, args) => {
+    console.log(`start logExecutionTime`)
     console.log(`Método decorado: ${property}`);
     console.log(`Argumentos do método: ${args}`);
     console.time(property);
     const result = method(...args);
     console.timeEnd(`Resultado do método: ${result}`);
+    console.log(`end logExecutionTime`)
     return result;
 };
 
 export const inspectMethod = (method, property, args) => {
+    console.log(`start inspectMethod`)
     console.log(`Método decorado: ${property}`);
     console.log(`Argumentos do método: ${args}`);
     const result = method(...args);
     console.log(`resultado do método: ${result}`);
+    console.log(`end inspectMethod`)
     return result;
 }
