64e158d477f51884324f8bef67b8aa573d1667f5
Caitano <josiasmartinscaitano@gmail.com>
2022-12-04 15:51:02 -0300

    feat: implementado o memoizer em funcao recursiva

--- public/cap-04/app/app.js
+++ public/cap-04/app/app.js
@@ -7,4 +7,17 @@ function somaDoisNumeros(numero1, numero2) {
 const somaDoisNumerosM = memoizer(somaDoisNumeros);
 console.log(somaDoisNumerosM(5, 5))
 console.log(somaDoisNumerosM(10, 10))
-console.log(somaDoisNumerosM(5, 5))
+console.log(somaDoisNumerosM(5, 5));
+
+// função recursiva
+const fatorial = memoizer(n =>  {
+    if (n <= 1) return 1;
+    return n * fatorial(--n)
+});
+
+
+// const fatorialM = memoizer(factorial);
+const f1 = fatorial(4);
+const f2 = fatorial(3);
+console.log(f1)
+console.log(f2)
