cd1e4ce870e26aefe2845ad3d3a3231acfe470f7
Caitano <josiasmartinscaitano@gmail.com>
2022-12-04 15:39:16 -0300

    feat: implementado a logica do cache

--- public/cap-04/app/infra/memoizer.js
+++ public/cap-04/app/infra/memoizer.js
@@ -1,8 +1,19 @@
 export function memoizer(fn) {
+    const cache = new Map();
     // rest operators
     return (...args) => {
-        // spread operators
-        const result = fn(...args);
-        return result;
+        // transforma numa string
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            console.log(`Buscou do cache ${args}`);
+            return cache.get(key);
+        } else {
+            console.log(key);
+            // spread operators
+            const result = fn(...args);
+            console.log(`Não encontrou no cache ${key}. Adicionando no cache`);
+            cache.set(key, result)
+            return result;
+        }
     }
 }
